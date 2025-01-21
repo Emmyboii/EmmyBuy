c8efc9a06335f4af2b5693e91aa552e8f7864ba1
Olukoya Emmanuel <olukoyaemmanuel@example.com>
2025-01-21 09:59:21 +0100

    Deploying

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -4,12 +4,15 @@ const app = express()
 const multer = require("multer")
 const cors = require("cors")
 const path = require("path")
-const connectDb = require("./config/Dbconnection")
+const connectDb = require("../config/Dbconnection")
 
 require('dotenv').config()
 
 app.use(express.json())
 
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hello, World!, Backend is running!' });
+}
 
 const corsOptions = {
     origin: (origin, callback) => {
@@ -59,8 +62,8 @@ app.post("/upload", upload.single('product'), (req, res) => {
 });
 
 
-const productRoutes = require('./routes/productRoutes')
-const userRoutes = require('./routes/userRoutes')
+const productRoutes = require('../routes/productRoutes')
+const userRoutes = require('../routes/userRoutes')
 
 app.use("/product", productRoutes)
 app.use("/user", userRoutes)
